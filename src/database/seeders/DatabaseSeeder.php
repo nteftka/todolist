7bdcc0ff0f478095c2841d53cfdcccb2e77386bc
nteftka <s_konno0517@yahoo.co.jp>
2022-09-26 10:13:58 +0900

    test5

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -14,7 +14,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
+            FoldersTableSeeder::class,
             TasksTableSeeder::class,
+            UsersTableSeeder::class,
         ]);
     }
 }
